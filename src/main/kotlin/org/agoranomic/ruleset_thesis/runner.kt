349b99e3ce212d242107e3eec8ae394ffd0976f2
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-23 09:08:42 -0400

    Remove REMOVE_HIGH_FREQ_RULES

--- src/main/kotlin/org/agoranomic/ruleset_thesis/runner.kt
+++ src/main/kotlin/org/agoranomic/ruleset_thesis/runner.kt
@@ -5,10 +5,9 @@ import org.agoranomic.ruleset_thesis.dependency_map.mutableCopy
 import org.agoranomic.ruleset_thesis.dependency_parsing.ManualAdjustment
 import org.agoranomic.ruleset_thesis.dependency_parsing.parseDependencies
 import org.agoranomic.ruleset_thesis.parsing.parseRuleList
+import org.agoranomic.ruleset_thesis.printers.graphviz.*
 import java.io.File
 import java.nio.file.Files
-
-private const val REMOVE_HIGH_FREQ_RULES = true
 
 fun main() {
     val text = Files.readString(File("rules.txt").toPath())
@@ -19,19 +18,19 @@ fun main() {
     )
     val ruleByNumber = rules.associateBy { it.number }
 
-    val dependencyMap = ManualAdjustment.stripMissingStrongDependencies(
+    val totalDependencyMap = ManualAdjustment.stripMissingStrongDependencies(
         ManualAdjustment.adjustDependencies(
             parseDependencies(rules)
         )
-    ).mutableCopy().also {
-        if (REMOVE_HIGH_FREQ_RULES) {
-            it.removeRule(2152)
-            it.removeRule(869)
-            it.removeRule(2141)
-            it.removeRule(478)
-            it.removeRule(101)
-            it.removeRule(1023)
-        }
+    )
+
+    val shortDependencyMap = totalDependencyMap.mutableCopy().also {
+        it.removeRule(2152)
+        it.removeRule(869)
+        it.removeRule(2141)
+        it.removeRule(478)
+        it.removeRule(101)
+        it.removeRule(1023)
     }.immutableCopy()
 
     // Doing stuff with dependencyMap is left as an exercise to the reader
