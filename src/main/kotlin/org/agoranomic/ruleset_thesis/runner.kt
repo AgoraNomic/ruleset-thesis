f3f6b2f810bb93ef9693858fa679cea2320ea800
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-24 17:32:18 -0400

    Add code to generate output used in thesis

--- src/main/kotlin/org/agoranomic/ruleset_thesis/runner.kt
+++ src/main/kotlin/org/agoranomic/ruleset_thesis/runner.kt
@@ -1,12 +1,14 @@
 package org.agoranomic.ruleset_thesis
 
-import org.agoranomic.ruleset_thesis.dependency_map.immutableCopy
-import org.agoranomic.ruleset_thesis.dependency_map.mutableCopy
+import org.agoranomic.ruleset_thesis.dependency_map.*
 import org.agoranomic.ruleset_thesis.dependency_parsing.ManualAdjustment
 import org.agoranomic.ruleset_thesis.dependency_parsing.parseDependencies
 import org.agoranomic.ruleset_thesis.parsing.parseRuleList
 import org.agoranomic.ruleset_thesis.printers.graphviz.*
-import java.io.File
+import org.agoranomic.ruleset_thesis.printers.toCircularDependencyRoutes
+import org.agoranomic.ruleset_thesis.printers.toDependencyCounts
+import org.agoranomic.ruleset_thesis.printers.toDependentCounts
+import org.agoranomic.ruleset_thesis.printers.toUnderpoweredDependencies
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -31,6 +33,18 @@ fun main() {
         it.removeRule(1023)
     }.immutableCopy()
 
-    // Doing stuff with dependencyMap is left as an exercise to the reader
-    // Check out the "printers" and dependency_map/analysis.kt for ideas.
+    outputGraph("total", toGraphvizSimple(rules, totalDependencyMap))
+    outputGraph("simplified", toGraphvizSimple(rules, shortDependencyMap))
+    outputGraph("cycles", toGraphvizColoredOnCircularDependencies(rules, shortDependencyMap))
+    outputGraph("leaves", toGraphvizColoredOnLeaves(rules, shortDependencyMap))
+    outputGraph("without_circular", toGraphvizSimple(rules, totalDependencyMap.withoutCircularDependencies()))
+
+    outputText("dependent_counts", toDependentCounts(rules, totalDependencyMap))
+    outputText("dependency_counts", toDependencyCounts(rules, totalDependencyMap))
+    outputText("circular_dependencies", toCircularDependencyRoutes(totalDependencyMap))
+    outputText("underpowered_dependencies", toUnderpoweredDependencies(rules, totalDependencyMap))
+
+    for (rule in listOf(2125, 869, 1023, 478)) {
+        outputText("dependents_$rule", shortDependencyMap.directDependentsOf(rule).joinToString("\n"))
+    }
 }
